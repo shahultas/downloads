52e7aff1d7bc280d7efe994477b26810938ecc69
Shahultas <tashahul8@gmail.com>
2024-01-03 09:04:52 +0530

    hello

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'asset')));
+
+app.get('/', (req, res)=> {
+res.send('<h1>Homepage</h1><br><br><img src="images/images.png" /><br><br><a href="/download"> Download </a>');
+});
+
+
+app.get('/download', (req, res)=> {
+res.download(path.join(__dirname, 'asset/images/images.png'));
+});
+
+
+app.listen(port, ()=> {
+console.log(`server connected at ${port}`);
+});
